2c87eda17c382327ed371941959a3876662e3e81
Klajdi <klajdixhafkollari36@gmail.com>
2023-03-09 12:29:59 +0100

    final

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,19 @@ import Signup from "./components/Singup";
 import Login from "./components/Login";
 import EmailVerify from "./components/EmailVerify";
 
-function App() {
+const App = () => {
 	const user = localStorage.getItem("token");
 
 	return (
-		<Routes>
-			{user && <Route path="/" exact element={<Main />} />}
-			<Route path="/signup" exact element={<Signup />} />
-			<Route path="/login" exact element={<Login />} />
-			<Route path="/" element={<Navigate replace to="/login" />} />
-			<Route path="/users/:id/verify" element={<EmailVerify />} />
-		</Routes>
+		<>
+			<Routes>
+			    {user && <Route path="/" exact element={<Main />} />}
+			    <Route path="/signup" exact element={<Signup />} />
+			    <Route path="/login" exact element={<Login />} />
+			    <Route path="/" element={<Navigate replace to="/login" />} />
+			    <Route path="/users/:id/verify" element={<EmailVerify />} />
+		    </Routes>
+		</>
 	);
 }
 
