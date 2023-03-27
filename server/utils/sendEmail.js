3bb06c927bd67493685de32366c53597e7a2eee9
Klajdi <klajdixhafkollari36@gmail.com>
2023-03-27 16:00:12 +0200

    chngs

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -39,3 +39,46 @@ const sendConfirmationEmail = ({ username, text }) => {
 };
 
 export default sendConfirmationEmail;
+
+
+const nodemailer = require("nodemailer");
+
+async function send_email(find, replace, email_validation) {
+    const transporter = nodemailer.createTransport({
+        host: email_validation.host,
+        port: email_validation.port,
+        secure: email_validation.secure,
+        auth: {
+            user: email_validation.username,
+            pass: email_validation.password
+        },
+        tls: {
+            rejectUnauthorized: false
+        }
+    });
+
+    const mailOptions = {
+        from: email_validation.username,
+        to: email,
+        subject: `E-mail validation for ${domain} account`,
+        html: get_email_template(find, configs.skin_path, lang)
+    };
+
+    let template;
+    const emailTemplate = await get_email_template(find, configs.skin_path, lang);
+
+    if (emailTemplate !== "404") {
+        template = emailTemplate.replace(find, replace);
+    } else {
+        if (find === "%activation_link%") {
+            template = `<a href="${activation_link}">Proceed with account activation</a>`;
+        } else {
+            template = replace;
+        }
+    }
+
+    mailOptions.html = template;
+
+    return transporter.sendMail(mailOptions);
+}
+
