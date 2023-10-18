77703916d5a89acaa07b972cd62f228a6a862442
Klajdi <klajdixhafkollari36@gmail.com>
2023-10-18 11:56:45 +0200

    commit 4

---
+++ server/src/clients/db/index.ts
@@ -0,0 +1,41 @@
+import config from "config";
+import { Sequelize } from "sequelize";
+import log from "../../utils/logger";
+
+const { 
+    host: dbHost, 
+    user: dbUser, 
+    password: dbPassword, 
+    database: dbName 
+} = config.get<{
+    host: string,
+    user: string,
+    password: string,
+    database: string
+}>("mysql");
+
+const dbDriver = "mysql";
+
+const sequelizeConnection = new Sequelize(dbName, dbUser, dbPassword, {
+    host: dbHost,
+    dialect: dbDriver,
+    logging: false,
+    define: {
+        timestamps: false
+    },
+    query: {
+        raw: true
+    }
+});
+
+const connectToDb = async () => {
+    try {
+        await sequelizeConnection.authenticate();
+        log.info("[db]: Successfully connected to database");
+    } catch (error) {
+        log.error(`Failed to connect to database: ${error}`);
+        return error;
+    }
+};
+
+export { sequelizeConnection, connectToDb };
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,39 @@
 import express, { Express } from "express";
 import config from "config";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import { connectToDb } from "./clients/db";
+import log from "./utils/logger";
 
 const { port, prefix } = config.get<{ port: number, prefix: string }>("app");
+const { cors_url } = config.get<{ cors_url: string }>("cors");
 
 const app: Express = express();
 
+// use express middleware for easier cookie handling
+const corsOptions = {
+    origin: cors_url,
+    credentials: true
+};
+
 app.disable("x-powered-by");
-app.use(cors());
+app.use(cors(corsOptions));
 app.options("*", cors());
+app.use(cookieParser());
 app.use(express.json({ limit: "10mb" }));
 
-app.listen(port, () => { 
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+// start server only when we have valid connection
+const startServer = async () => {
+    try {
+        await connectToDb();
+        app.listen(port, () => { 
+            log.info(`[server]: Server is running at http://localhost:${port}`);
+        });
+    } catch (error) {
+        log.error(`Cannot connect to the server: ${error}`);
+    }
+};
+
+startServer().catch((error) => {
+    log.error(`Invalid database connection: ${error}`);
 });
