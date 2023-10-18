fcdd978d562a1652be545c69d5cf522dc2f688c3
Klajdi660 <klajdixhafkollari36@gmail.com>
2023-10-18 21:35:06 +0200

    fixed

--- server/src/api/User/createUser.ts
+++ server/src/api/User/createUser.ts
@@ -21,7 +21,6 @@ const createUser = async (data: UserTypesParams) => {
         username,
         firstName, 
         lastName,
-        token: "1234abcd"
     });
 
     const saveUser = await newUser
@@ -36,20 +35,24 @@ export const createUserHandler = async (data: UserTypesParams) => {
     const { email, username } = data;
 
     // check user in database
-    // const existingUser: any = await getUsersByEmailOrUsername(email, username);
+    const existingUser: any = await getUsersByEmailOrUsername(email, username);
 
-    // if (existingUser) {
-    //     if (existingUser.email === email && existingUser.username === username) {
-    //         return { error: true, message: "Email and username already exist" };
-    //     } else if (existingUser.email === email) {
-    //         return { error: true, message: "Email already exists" };
-    //     } else if (existingUser.username === username) {
-    //         return { error: true, message: "Username already exists" };
-    //     }
-    // }
+    if (existingUser) {
+        if (existingUser.email === email && existingUser.username === username) {
+            return { error: true, message: "Email and username already exist" };
+        } else if (existingUser.email === email) {
+            return { error: true, message: "Email already exists" };
+        } else if (existingUser.username === username) {
+            return { error: true, message: "Username already exists" };
+        }
+    }
 
-    // // insert user in database
-    // const user = await createUser(data);
-    // console.log('user :>> ', user);
+    // insert user in database
+    const user = await createUser(data);
+    console.log('user :>> ', user);
+    if (!user) {
+        return { error: true, message: "User can't be created" };
+    }
+
     const mail = await sendEmail();
 };
