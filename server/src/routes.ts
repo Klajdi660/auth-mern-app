bddd08564c30d3e2ee3afebaad69512993db0906
Klajdi <klajdixhafkollari36@gmail.com>
2023-12-13 17:01:48 +0100

    chngs3

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,12 +2,12 @@ import { Router } from "express";
 import config from "config";
 import { userRoutes } from "./api/User";
 import { authRoutes } from "./api/Auth";
+
 const { prefix } = config.get<{ prefix: string }>("app");
 
 const routes = Router();
 
 routes.use(`${prefix}/auth`, authRoutes)
-// routes.use(`${prefix}/auth`, userRoutes);
-
+routes.use(`${prefix}/users`, userRoutes);
 
 export default routes;
